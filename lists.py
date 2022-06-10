e2c42b501d74bc9a91dc4a0cd3dad7f60874d2f8
sergemr <sergemr@gmail.com>
2022-06-10 07:04:22 -0600

    lists examples

---
+++ lists.py
@@ -0,0 +1,12 @@
+
+names = ["hugo", "Paco", "Luis"]
+names[0] = "Hugo!"
+print(names)
+print(names[0])
+names.append("Donald")
+names.insert(0, "Duck")
+print(names[2:4])
+print(names)
+names.remove("Paco")
+print(names)
+print("hugo" in names)
